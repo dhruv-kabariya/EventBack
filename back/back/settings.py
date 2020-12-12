82040e8de85e096e38e97802dc6e4aeca1989a84
Dhruv Kabariya <dhruvkabariya1@gmail.com>
2020-12-12 14:16:12 +0530

    some chages test 1

--- back/back/settings.py
+++ back/back/settings.py
@@ -28,14 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG',default=False)
+DEBUG = env.bool('DEBUG',default=True)
 
 ALLOWED_HOSTS = [
-    "https://eventback.herokuapp.com/",
-    '0.0.0.0',
-    "127.0.0.1"
-    
-
 ]
 
 
