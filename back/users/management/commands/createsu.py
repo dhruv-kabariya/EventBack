58157029240f96c63c350cbe75d7549ba38013e1
Dhruv Kabariya <dhruvkabariya1@gmail.com>
2020-12-13 19:21:44 +0530

    aws db test 15

--- back/users/management/commands/createsu.py
+++ back/users/management/commands/createsu.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from users.models import Student
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "dhruvkabaiya1@gmail.com", "admin")
+        if not Student.objects.filter(username="admin").exists():
+            Student.objects.create_superuser("admin", "dhruvkabaiya1@gmail.com", "admin")
